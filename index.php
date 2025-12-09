2b6cf616fbdbc8445342b6a20738d956051ccb73
kentos223 <159707649+kentos223@users.noreply.github.com>
2025-12-09 16:04:35 +0700

    update index

--- index.php
+++ index.php
@@ -11,7 +11,7 @@ $anggota = [
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Daftar Anggota & Tugas KSI</title>
+    <title>Daftar Anggota & Tugas KSI 2025</title>
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
 
